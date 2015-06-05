d72997e0701381af5b3791a94c4d0f8e10824cec
Elisabeth Anderson <beth@vintur.cc>
2015-06-05 15:00:09 +0100

    Updates

--- app/controllers/donk_controller.rb
+++ app/controllers/donk_controller.rb
@@ -1,5 +1,9 @@
 class DonkController < ApplicationController
 
+    def index
+
+    end
+
     def kick
         loop("kick", params)
     end
@@ -9,12 +13,12 @@ class DonkController < ApplicationController
     end
 
     private
-    def loop(type, params)
-        bmp = params.has_key?(:bmp) ? params[:bmp] : 120
-        pattern = params.has_key?(:pattern) ? params[:pattern] : "xxxx"
+        def loop(type, params)
+            bmp = params.has_key?(:bmp) ? params[:bmp] : 120
+            pattern = params.has_key?(:pattern) ? params[:pattern] : "xxxx"
 
-        merge_samples
+            merge_samples
 
-        send_file "#{Rails.public_path}/clips/#{type}.mp3", :type=>"audio/mp3", :filename => "#{type}_#{bmp}_#{pattern}.mp3"
-    end
+            send_file "#{Rails.public_path}/clips/#{type}.mp3", :type=>"audio/mp3", :filename => "#{type}_#{bmp}_#{pattern}.mp3"
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'donk#index'
+
   get 'kick' => 'donk#kick'
   get 'clap' => 'donk#clap'
-  get 'donk' => 'donk#donk'
 end
